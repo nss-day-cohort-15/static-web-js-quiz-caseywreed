46642c99f38e895e68a837b244148f8304f55520
Casey Reed <casey.w.reed@gmail.com>
2016-07-20 22:41:11 -0500

    Changed the function to use an object. Whoops.

--- javascript/main.js
+++ javascript/main.js
@@ -1,18 +1,29 @@
 
 var button = document.getElementById("button");
 button.addEventListener("click", makeTree);
+document.addEventListener("keydown", enter);
 
-function makeTree() {
 
-	var character = document.getElementById("character").value;
-	var height = document.getElementById("height").value;
+function enter(e) {
+  if (13 == e.keyCode) {
+     makeTree()
+  }
+}
+
+var tree = {}
+
+// console.log(tree)
+
+function makeTree(tree) {
+  tree.height = document.getElementById("height").value;
+  tree.character = document.getElementById("character").value;
 	var counter = 1;
 
-	if (character === "" || height === "")
-		alert("ERROR: Both fields must have a value")
+	if (tree.character === "" || tree.height === "")
+		alert("ERROR: Both fields must have a value. You've ruined Christmas.")
 
-	for (i = 0; i < height; i++) {
-		console.log(" ".repeat(height-i) + character.repeat(counter))
+	for (i = 0; i < tree.height; i++) {
+		console.log(" ".repeat(tree.height-i) + tree.character.repeat(counter))
 		counter += 2;
 	}
 }
